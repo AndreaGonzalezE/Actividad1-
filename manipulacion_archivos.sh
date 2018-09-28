29d72749289b2eefd115c6b2c29e300ada441c57
root <root@localhost.localdomain>
2018-09-27 23:19:37 -0700

    Archivos de practica

---
+++ manipulacion_archivos.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+touch temp.txt
+whoami > temp.txt
+mkdir -p whoamidir
+cd whoamidir
+cp ../temp.txt whoami.txt
+
+cd ..
+ip address >temp.txt
+mkdir -p networkinfo
+cd networkinfo
+mv ../temp.txt net-info.txt
+
+cd..
+mkdir -p temp 
+cd temp 
+touch temp.txt 
+Andy >temp.txt
+
+cat temp.txt
+\rm temp.txt
+cd..
+\rmdir temp/
